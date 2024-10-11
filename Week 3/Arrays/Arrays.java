ce4221cec6edf4ff303ee8e5b2430922e0bfbb50
LiliWuu <lilianwu08@gmail.com>
2024-10-10 17:02:39 -0700

    completed arrays activity

---
+++ Week 3/Arrays/Arrays.java
@@ -0,0 +1,8 @@
+public class Arrays {
+    public static void main(String[] args) {
+        String[] arr = {"Software", "Tomiko Itooka", "Up-A-Creek Robotics", "Aerospace Valley Regional 2019"};
+        for (String obj: arr) {
+            System.out.println(obj);
+        }
+    }
+}
