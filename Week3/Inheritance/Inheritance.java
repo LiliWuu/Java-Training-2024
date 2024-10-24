ab463350a9c96816470ee4650b9cb5aa0ae30ef7
LiliWuu <lilianwu08@gmail.com>
2024-10-24 16:11:52 -0700

    idk

--- Week3/Inheritance/Inheritance.java
+++
@@ -1,9 +0,0 @@
-package Week3.Inheritance;
-
-public class Inheritance {
-    public static void main(String[] args) {
-        Child p1 = new Child("Max", "Wu", "Brown");
-        System.out.println(p1.getFirstLastName());
-
-    }
-}
