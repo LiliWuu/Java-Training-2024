a0e4971cbe0b1b17846ea022b2339e94e69f5c0b
LiliWuu <lilianwu08@gmail.com>
2024-10-24 16:00:19 -0700

    polymorphism

---
+++ Week3/Polymorphism/Calculator.java
@@ -0,0 +1,11 @@
+package Week3.Polymorphism;
+
+public class Calculator {
+    public int divide(int a, int b) {
+        return a / b;
+    }
+    public double divide(double a, double b) {
+        return a / b;
+    }
+    
+}
