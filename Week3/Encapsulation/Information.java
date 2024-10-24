ad85572847ea5b0d0f4eb5d87135664538da7181
LiliWuu <lilianwu08@gmail.com>
2024-10-24 15:57:16 -0700

    encapsulation

---
+++ Week3/Encapsulation/Information.java
@@ -0,0 +1,33 @@
+package Week3.Encapsulation;
+
+public class Information {
+    private String name;
+    private int age;
+    private String gender;
+
+    public String getName() {
+        return name;
+    }
+
+    public String setName(String x) {
+       return name = x;
+    }
+
+
+    public int getAge() {
+        return age;
+    }
+
+    public int setAge(int x) {
+       return age = x;
+    }
+    
+    public String getGender() {
+        return gender;
+    }
+
+    public String setGender(String x) {
+        return name = x;
+    }
+}
+
